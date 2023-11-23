8687d7fc1f2b30f36244d4d16a19715bc9720c86
vishnupandey <vishnupandey.work@gmail.com>
2023-11-23 18:42:57 +0530

    Updating some packages

--- cypressTestAutomation/cypress.config.ts
+++ cypressTestAutomation/cypress.config.ts
@@ -1,9 +1,12 @@
 const { defineConfig } = require("cypress");
+import * as path from 'path'
+import * as fs from 'fs'
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      //require('cypress-moch')
     },
     specPattern:"cypress/e2e/**/*.spec.ts"
   },
