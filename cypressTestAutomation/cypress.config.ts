d815507a838f17be6c20629bb73adc7f7172fde9
vishnupandey <vishnupandey.work@gmail.com>
2023-11-27 19:50:18 +0530

    connecting to cloud and some minor changes

--- cypressTestAutomation/cypress.config.ts
+++ cypressTestAutomation/cypress.config.ts
@@ -1,13 +1,22 @@
 const { defineConfig } = require("cypress");
 import * as path from 'path'
-import * as fs from 'fs'
+import * as fs from 'fs-extra'
 
 module.exports = defineConfig({
+  projectId: "xmhkmk",
   reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
       require('cypress-mochawesome-reporter/plugin')(on);
+
+      function getConfigurationByFile(filename) {
+        const pathToConfigFile = path.resolve('cypress/config', `${filename}.json`)
+        return fs.readJSON(pathToConfigFile)
+      }
+
+      const file = config.env.configFile || 'default'
+      return getConfigurationByFile(file)
     },
     specPattern: "cypress/e2e/**/*.spec.ts"
   },
--- cypressTestAutomation/cypress/e2e/firstTest.spec.ts
+++ cypressTestAutomation/cypress/e2e/firstTest.spec.ts
@@ -1,6 +1,12 @@
 describe('myTest', () => {
 
     it('', () => {
-        cy.visit("https://www.amazon.in")
+        //cy.visit("https://www.amazon.in")
+        //cy.visit('https://www.google.com')
+        //cy.wait(10000)
+        cy.visit('/')
+        cy.get('#username').clear().type('dummyUser')
+        cy.get('#password').clear().type('dummyPassword')
+        cy.get('#log-in').click()
     })
 })
